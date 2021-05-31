97a30b139197cadc2f41e8eda716c13270fe55de
Priyobroto Roy <priyocriro7@gmail.com>
2021-05-31 15:58:54 +0530

    detail & navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DetailsComponent } from './details/details.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { AddOnPageComponent } from './add-on-page/add-on-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DetailsComponent,
+    LogInComponent,
+    AddOnPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+  ],
+  exports:[
+    DetailsComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
