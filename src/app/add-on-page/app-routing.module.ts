bfd0d1b3428aa2df7cf0e5084afacae29abcbbd5
Priyobroto Roy <priyocriro7@gmail.com>
2021-06-01 20:28:50 +0530

    metrotune

--- src/app/add-on-page/app-routing.module.ts
+++ src/app/add-on-page/app-routing.module.ts
@@ -1,16 +1,16 @@
-import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
-import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+//import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+//import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
-import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+//import { EmployeeListComponent } from './employee-list/employee-list.component';
+//import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'employee', pathMatch: 'full' },
-  { path: 'employees', component: EmployeeListComponent },
-  { path: 'add', component: CreateEmployeeComponent },
-  { path: 'update/:id', component: UpdateEmployeeComponent },
-  { path: 'details/:id', component: EmployeeDetailsComponent },
+  //{ path: 'employees', component: EmployeeListComponent },
+  //{ path: 'add', component: CreateEmployeeComponent },
+  //{ path: 'update/:id', component: UpdateEmployeeComponent },
+  //{ path: 'details/:id', component: EmployeeDetailsComponent },
 ];
 
 @NgModule({
